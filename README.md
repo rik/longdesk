ae35eb0e646e24823607fba9bbd0c9205e2e2ad0
Anthony Ricaud <anthony@ricaud.me>
2011-07-05 16:55:50 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+A simple add-on that adds a link to the longdesc under images that provides one.
+
+Maybe you want to look at this other extension for longdesc https://addons.mozilla.org/en-US/firefox/addon/longdesc/
+
+The source code lives at https://github.com/Rik/longdesk
+
+*Disclaimer:* I have no opinion on whether the longdesc attribute should or should not exist.
+I'm just tired of having to look through the source code every time I want to see a friend's explanation of a picture.
+This add-on has only been tested on http://www.nota-bene.org/Mise-en-abyme-a-carreaux
+
+Also, this is my first add-on ever so…
---
+++ data/pagemod.js
@@ -0,0 +1,30 @@
+(function () {
+    var imgs = document.querySelectorAll('img[longdesc]');
+    
+    if (imgs.length == 0)
+        return;
+    
+    self.postMessage('load-styles');
+    self.on('message', function (data) {
+        if (data.styles) {
+            var style = document.createElement('style');
+            style.appendChild(document.createTextNode(data.styles));
+            document.head.appendChild(style);
+        }
+    })
+    
+    for (var i=0, il=imgs.length; i < il; i++) {
+        var img = imgs[i];
+        
+        var a = document.createElement('a');
+        a.classList.add('longdesc-addon-link');
+        a.href = imgs[i].getAttribute('longdesc');
+        a.appendChild(document.createTextNode('Description'));
+        
+        var top = img.offsetTop + img.height;
+        a.style.top = top + "px";
+        a.style.left = img.offsetLeft + "px";
+        
+        img.parentNode.insertBefore(a, img.nextSibling);
+    }
+})();
