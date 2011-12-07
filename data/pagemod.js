485809bca7ee806e245eb54ac0503a33148b535e
notabene <stephane.deschamps@gmail.com>
2011-12-07 15:41:26 +0100

    if longdesc attribute longer than 0 then generate a longdesc tab - otherwise don't. code off the top of my head, cannot test it right now, pushing 'as is'.

--- data/pagemod.js
+++ data/pagemod.js
@@ -15,16 +15,18 @@
     
     for (var i=0, il=imgs.length; i < il; i++) {
         var img = imgs[i];
+        if(img.getAttribute("longdesc").toString().length>0) {
+            var a = document.createElement('a');
+            a.classList.add('longdesc-addon-link');
+            a.href = imgs[i].getAttribute('longdesc');
+            a.appendChild(document.createTextNode('Description'));
         
-        var a = document.createElement('a');
-        a.classList.add('longdesc-addon-link');
-        a.href = imgs[i].getAttribute('longdesc');
-        a.appendChild(document.createTextNode('Description'));
+            var top = img.offsetTop + img.height;
+            a.style.top = top + "px";
+            a.style.left = img.offsetLeft + "px";
         
-        var top = img.offsetTop + img.height;
-        a.style.top = top + "px";
-        a.style.left = img.offsetLeft + "px";
+            img.parentNode.insertBefore(a, img.nextSibling);
         
-        img.parentNode.insertBefore(a, img.nextSibling);
+        }
     }
 })();
