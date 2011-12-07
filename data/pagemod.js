fdb8e4c80127123a595dfb81b2c520ad422657fe
notabene <stephane.deschamps@gmail.com>
2011-12-07 16:16:44 +0100

    early return instead of indent as per Rik's request

--- data/pagemod.js
+++ data/pagemod.js
@@ -15,18 +15,18 @@
     
     for (var i=0, il=imgs.length; i < il; i++) {
         var img = imgs[i];
-        if(img.getAttribute("longdesc").toString().length>0) {
-            var a = document.createElement('a');
-            a.classList.add('longdesc-addon-link');
-            a.href = imgs[i].getAttribute('longdesc');
-            a.appendChild(document.createTextNode('Description'));
+        if(img.getAttribute("longdesc").toString().length<1) return;
         
-            var top = img.offsetTop + img.height;
-            a.style.top = top + "px";
-            a.style.left = img.offsetLeft + "px";
+        var a = document.createElement('a');
+        a.classList.add('longdesc-addon-link');
+        a.href = imgs[i].getAttribute('longdesc');
+        a.appendChild(document.createTextNode('Description'));
         
-            img.parentNode.insertBefore(a, img.nextSibling);
+        var top = img.offsetTop + img.height;
+        a.style.top = top + "px";
+        a.style.left = img.offsetLeft + "px";
         
-        }
+        img.parentNode.insertBefore(a, img.nextSibling);
+        
     }
 })();
